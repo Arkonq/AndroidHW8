3c49e09a42be2dc49cff4414e09c37a6ecbaca99
arkonq@gmail.com <arkonq@gmail.com>
2021-04-05 11:07:55 +0600

    Initial commit

---
+++ app/src/main/java/com/example/androidhw8/StudentInformationFragment.kt
@@ -0,0 +1,13 @@
+package com.example.androidhw8
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class StudentInformationFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_student_information, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/androidhw8/StudentsFragment.kt
@@ -0,0 +1,14 @@
+package com.example.androidhw8
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class StudentsFragment() : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_students, container, false)
+    }
+}
